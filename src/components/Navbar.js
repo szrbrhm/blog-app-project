dfbdaa01485c2a36a032cd8ab98396dcfe358ddc
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-09-30 20:24:27 +0200

    create navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,96 @@
-import React from "react";
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import CwLogo from '../assets/cw.jpeg'
 
-const Navbar = () => {
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+    display: "none", fontFamily: "Girassol",[theme.breakpoints.up("sm")]: {display: "block",  },
+    "& span": {fontSize: 30,color: "wheat"},    
+  },
+  
+  menuButton: { marginRight: theme.spacing(2),  
+  },
+  appbar: {
+    backgroundColor:'#046582'
+  },
+  logo:{
+    width: 40,
+  }
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
+
+
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
   return (
-    <div>
-      <h2>NAVBAR</h2>
+    <div className={classes.root}>
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+            <a href="www.clarusway.com"><img src={CwLogo} alt="" className={classes.logo} /></a>
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            <span>{'< Sezer /> BLOG'}</span>
+          </Typography>
+          
+            <div>
+              <IconButton
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit"
+              >
+                <AccountCircle  style={{fontSize:'40px'}}/>
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={open}
+                onClose={handleClose}
+              >
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>My account</MenuItem>
+              </Menu>
+            </div>
+          
+        </Toolbar>
+      </AppBar>
     </div>
   );
-};
+}
 
-export default Navbar;
