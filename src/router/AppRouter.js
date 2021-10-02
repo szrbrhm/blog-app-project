c8b5b82828b8608ef59cb56c4044a27b5be2139c
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-02 11:11:59 +0200

    add privaterouter

--- src/router/AppRouter.js
+++ src/router/AppRouter.js
@@ -3,6 +3,12 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Login from '../pages/Login'
 import Register from "../pages/Register"
+import Dashboard  from "../pages/Dashboard"
+import NewBlog from '../pages/NewBlog'
+import Detail from '../pages/Detail'
+import UpdateBlog from '../pages/UpdateBlog'
+import PrivateRouter from "./PrivateRouter";
+import Profile from '../pages/Profile'
 
 const AppRouter = () => {
     return (
@@ -11,6 +17,12 @@ const AppRouter = () => {
             <Switch>
                 <Route path="/login" component={Login}/>
                 <Route path="/register" component={Register}/>
+                <Route path="/" component={Dashboard}/>
+
+                <PrivateRouter path="/profile" component={Profile}/>
+                <PrivateRouter path="/new-blog"  component={NewBlog}/>
+                <PrivateRouter path="/update-blog/:id" component={UpdateBlog}/>
+                <PrivateRouter path="/detail/:id" component={Detail}/>
             </Switch>
             
         </Router>
