4c58234ff64a91b27570319a941818b1e3e23290
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-02 15:12:21 +0200

    add toastcontainer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
+
 import "./App.css";
 import Navbar from "./components/Navbar";
 import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
+import { ToastContainer } from "react-toastify";
+
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
       <AppRouter/>
+      <ToastContainer/>
 
         </AuthContextProvider>
       
