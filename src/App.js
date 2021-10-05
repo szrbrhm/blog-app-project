ea83a6b34591df6a26f55486afa4094ad559e20e
szrbrhm <81762608+szrbrhm@users.noreply.github.com>
2021-10-05 20:37:46 +0200

    add comp

--- src/App.js
+++ src/App.js
@@ -4,14 +4,17 @@ import Navbar from "./components/Navbar";
 import AuthContextProvider from "./context/AuthContextProvider";
 import AppRouter from "./router/AppRouter";
 import { ToastContainer } from "react-toastify";
+import { BlogContextProvider } from "./context/BlogContextProvider";
 
 
 function App() {
   return (
     <div className="App">
       <AuthContextProvider>
+        <BlogContextProvider>
       <AppRouter/>
       <ToastContainer/>
+      </BlogContextProvider>
 
         </AuthContextProvider>
       
